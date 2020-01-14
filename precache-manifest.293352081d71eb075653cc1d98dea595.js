6032e568bf771b52702ef3e1a1cf3a28a81bbcb2
ArseniyX <jkl@usa.com>
2020-01-14 14:45:53 +0200

    Updates

---
+++ precache-manifest.293352081d71eb075653cc1d98dea595.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f53447a69e81c0394e0e8628e022c17",
+    "url": "/my-robots/index.html"
+  },
+  {
+    "revision": "2056a058c586019e6233",
+    "url": "/my-robots/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "7f23c7faf5e58925bbf4",
+    "url": "/my-robots/static/css/main.a6f53f68.chunk.css"
+  },
+  {
+    "revision": "2056a058c586019e6233",
+    "url": "/my-robots/static/js/2.e32ba0ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-robots/static/js/2.e32ba0ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f23c7faf5e58925bbf4",
+    "url": "/my-robots/static/js/main.2b613e51.chunk.js"
+  },
+  {
+    "revision": "93bdfc37819ccd91445a",
+    "url": "/my-robots/static/js/runtime-main.f957ecbc.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/my-robots/static/media/SEGA.82e09f78.TTF"
+  }
+]);
